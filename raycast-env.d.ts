263a06b2b4e09cb20b90628f1df34348689760c1
Millin Gabani <gabanimillin@gmail.com>
2023-08-16 18:49:08 -0400

    added extension code

---
+++ raycast-env.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `smart-capture-into-obdisian` command */
+  export type SmartCaptureIntoObdisian = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `smart-capture-into-obdisian` command */
+  export type SmartCaptureIntoObdisian = {}
+}
+
+
+declare module "swift:*" {
+  function run<T = unknown, U = any>(command: string, input?: U): Promise<T>;
+  export default run;
+	export class SwiftError extends Error {
+    stderr: string;
+    stdout: string;
+  }
+}
